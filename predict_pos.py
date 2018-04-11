5e434008e6327214839fedfd149060df44c99ea9
yunhua98 <yunhua98@gmail.com>
2018-04-11 16:14:27 -0500

    Add label frequency graphs

--- predict_pos.py
+++ predict_pos.py
@@ -5,26 +5,27 @@ import numpy as np
 import os, csv
 
 # deserialize model
-# fmods = open(os.path.join("./data", "pos_model_20val.json"), "rb")
+fmods = open(os.path.join("./data", "fast_pos_model.json"), "rb")
 # fmods = open(os.path.join("./data", "pos_model_25val.json"), "rb")
 # fmods = open(os.path.join("./chunking", "chunking_model.json"), "rb")
-fmods = open(os.path.join("./ner", "ner_model.json"), "rb")
+# fmods = open(os.path.join("./ner", "ner_model.json"), "rb")
 model_json = fmods.read().decode()
 fmods.close()
 model = model_from_json(model_json)
-# model.load_weights(os.path.join("./data/checkpoints_small", "small_pos_weights.50-0.65.hdf5"))
+model.load_weights(os.path.join("./data/checkpoints_fast", "pos_weights.50-0.42.hdf5"))
 # model.load_weights(os.path.join("./data/checkpoints", "large_pos_weights.17-0.39.hdf5"))
 # model.load_weights(os.path.join("./data/checkpoints3", "large_pos_weights.49-0.37.hdf5"))
 # model.load_weights(os.path.join("./data/checkpoints_hard_sig3", "large_pos_weights.28-1.15.hdf5"))
 # model.load_weights(os.path.join("./data/checkpoints_large", "large_pos_weights.36-0.28.hdf5"))
 # model.load_weights(os.path.join("./chunking/checkpoints", "chunking_weights.19-0.34.hdf5"))
-model.load_weights(os.path.join("./ner/checkpoints_augmented", "ner_weights.43-0.17.hdf5"))
-adamax = Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+# model.load_weights(os.path.join("./ner/checkpoints_augmented", "ner_weights.43-0.17.hdf5"))
+# adamax = Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+adamax = Adamax(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 model.compile(loss="categorical_crossentropy", optimizer=adamax)
 
 # create label lookup tool
 str_labels = []
-with open("./ner/ner_labels.csv", "r") as f:
+with open("./data/labels.csv", "r") as f:
     for label in f.readlines():
         str_labels.append(label.strip())
 
@@ -34,24 +35,24 @@ le.fit(str_labels)
 
 # predict
 # test_data = np.load("./data/100_split_features/features0.npy")
-test_data = np.load("./ner/train_features5.npy")
+test_data = np.load("./data/features.npy")
 
 
 
-### STUFF FOR AUGMENTING NER WITH POS FEATURES
+# ### STUFF FOR AUGMENTING NER WITH POS FEATURES
 
-pos_labels = []
-with open("./ner/pos_labels.csv", "r") as f:
-    for label in f.readlines():
-        pos_labels.append(label.strip())
+# pos_labels = []
+# with open("./ner/pos_labels.csv", "r") as f:
+#     for label in f.readlines():
+#         pos_labels.append(label.strip())
 
-le_pos = LabelEncoder()
-le_pos.fit(pos_labels)
+# le_pos = LabelEncoder()
+# le_pos.fit(pos_labels)
 
-pos_labels = le_pos.transform(pos_labels)[:test_data.shape[0]]
-test_data = np.column_stack((test_data, pos_labels))
+# pos_labels = le_pos.transform(pos_labels)[:test_data.shape[0]]
+# test_data = np.column_stack((test_data, pos_labels))
 
-### END STUFF
+# ### END STUFF
 
 
 one_hot_predictions = model.predict(test_data)
@@ -61,6 +62,6 @@ for prediction in one_hot_predictions:
 
 predicted_labels = le.inverse_transform(predictions)
 
-with open("./results/predictions/ner_train_augmented_predictions_5gram.csv", "w") as file:
+with open("./results/predictions/fast_pos_train_augmented_predictions_5gram.csv", "w") as file:
     wr = csv.writer(file, delimiter="\n", quoting=csv.QUOTE_NONE)
     wr.writerow(predicted_labels)
--- train_network.py
+++ train_network.py
@@ -12,35 +12,35 @@ import os
 # labels = np.load("./data/100_split_labels/labels0.npy")
 # features = np.load("./data/10_split_features3/features0.npy")
 # labels = np.load("./data/10_split_labels/labels0.npy")
-# features = np.load("./data/features.npy")
-# labels = np.load("./data/labels.npy")
+features = np.load("./data/features.npy")
+labels = np.load("./data/labels.npy")
 # features = np.load("./data/10_split_features/features0.npy")
 # labels = np.load("./data/labels0_shuffled.npy")
 # features = np.load("./chunking/features5.npy")
 # labels = np.load("./chunking/labels.npy")
-features = np.load("./ner/train_features5.npy")
-labels = np.load("./ner/train_ner_labels.npy")
+# features = np.load("./ner/train_features5.npy")
+# labels = np.load("./ner/train_ner_labels.npy")
 
-### STUFF FOR AUGMENTING NER WITH POS FEATURES
+# ### STUFF FOR AUGMENTING NER WITH POS FEATURES
 
-# create label lookup tool
-str_labels = []
-with open("./ner/pos_labels.csv", "r") as f:
-    for label in f.readlines():
-        str_labels.append(label.strip())
+# # create label lookup tool
+# str_labels = []
+# with open("./ner/pos_labels.csv", "r") as f:
+#     for label in f.readlines():
+#         str_labels.append(label.strip())
 
-le = LabelEncoder()
-le.fit(str_labels)
+# le = LabelEncoder()
+# le.fit(str_labels)
 
-pos_labels = le.transform(str_labels)[:features.shape[0]]
-features = np.column_stack((features, pos_labels))
+# pos_labels = le.transform(str_labels)[:features.shape[0]]
+# features = np.column_stack((features, pos_labels))
 
-### END STUFF
+# ### END STUFF
 
 # set up model
 model = Sequential([
     # input layer
-    Dense(128, input_shape=(501,), W_regularizer=l2(0.001)),
+    Dense(128, input_shape=(500,), W_regularizer=l2(0.001)),
     Activation("relu"),
     # Activation("hard_sigmoid"),
     Dropout(0.2),
@@ -50,26 +50,27 @@ model = Sequential([
     # Activation("hard_sigmoid"),
     Dropout(0.2),
     # output layer
-    # Dense(34), # POS
+    Dense(34), # POS
     # Dense(21), # Chunking
-    Dense(17), # NER
+    # Dense(17), # NER
     Activation("softmax")
     # Activation("relu"),
     # Activation("hard_sigmoid")
 ])
 
-adamax = Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+# adamax = Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+adamax = Adamax(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 model.compile(loss="categorical_crossentropy", optimizer=adamax)
 
 # save model structure
 model_struct = model.to_json()
-fmod_struct = open(os.path.join("./ner", "ner_model.json"), "wb")
+fmod_struct = open(os.path.join("./data", "fast_pos_model.json"), "wb")
 fmod_struct.write(model_struct.encode())
 fmod_struct.close()
 
 # train model
-checkpoint = ModelCheckpoint(os.path.join("./ner", "checkpoints_augmented",
-    "ner_weights.{epoch:02d}-{val_loss:.2f}.hdf5"), 
+checkpoint = ModelCheckpoint(os.path.join("./data", "checkpoints_fast",
+    "pos_weights.{epoch:02d}-{val_loss:.2f}.hdf5"), 
     monitor="val_loss", save_best_only=True, mode="min")
 # checkpoint = ModelCheckpoint(os.path.join("./data", "checkpoints_large",
 #     "large_pos_weights.{epoch:02d}-{val_loss:.2f}.hdf5"), 
